5f54d3ab57b964b53cd5a5e020e08ea3363b2a1f
Oftond <161725771+Oftond@users.noreply.github.com>
2026-04-09 16:52:36 +0700

    Update models

---
+++ backend/Models/DTO/Auth/RegisterResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Mini_Marketplace.Models.DTO.Auth
+{
+    public class RegisterResponse
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string? FullName { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
